bfebfec2d33a5b5d8c3f8dec1d699efac725e06f
Joseph Kim <josek16@uci.edu>
2023-05-24 20:17:53 -0700

    setup functions

--- assets/models/Car.py
+++ assets/models/Car.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Car:
     def __init__(self, x: int, y: int):
         # Passed parameters
@@ -8,10 +11,57 @@ class Car:
         self.width = 1
         self.height = 1
         self.points = 0
+        self.dvel = 1
+        self.vel = 0
+        self.velX = 0
+        self.velY = 0
+        self.maxVel = 15
+        self.angle = math.radians(180)
+        self.soll_angle = self.angle
 
     def action(self, choice: int):
         match choice:
-            case 0:
-                break
+            case 7:
+                self.accelerate(self.dvel)
+                self.turn(-1)
 
-            
+            case 8:
+                self.accelerate(self.dvel)
+
+            case 9:
+                self.accelerate(self.dvel)
+                self.turn(1)
+
+            case 4:
+                self.turn(-1)
+
+            case 6:
+                self.turn(1)
+
+            case 1:
+                self.accelerate(-self.dvel)
+                self.turn(-1)
+
+            case 2:
+                self.accelerate(-self.dvel)
+
+            case 3:
+                self.accelerate(-self.dvel)
+                self.turn(1)
+
+            case _:
+                pass
+
+    def accelerate(self, dvel):
+        dvel = dvel * 2
+
+        self.vel = self.vel + dvel
+
+        if self.vel > self.maxvel:
+            self.vel = self.maxvel
+
+        if self.vel < -self.maxvel:
+            self.vel = -self.maxvel
+
+    def turn(self, dir):
+        self.soll_angle = self.soll_angle + dir * math.radians(15)
