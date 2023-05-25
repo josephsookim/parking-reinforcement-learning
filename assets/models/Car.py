a50f7843de54c2dc090b04260831a7aa23267a2c
Joseph Kim <josek16@uci.edu>
2023-05-24 20:07:41 -0700

    setup

---
+++ assets/models/Car.py
@@ -0,0 +1,15 @@
+class Car:
+    def __init__(self, x: int, y: int):
+        # Passed parameters
+        self.x = x
+        self.y = y
+
+        # Hard coded variables
+        self.width = 1
+        self.height = 1
+        self.points = 0
+
+    def action(self, choice: int):
+        match choice:
+            case 0:
+                break
