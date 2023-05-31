08fecb3da6e1cc09636eb24d087eaf2a2367bbae
Joseph Kim <josek16@uci.edu>
2023-05-30 19:58:35 -0700

    update goal collision and change line to rectangle

---
+++ assets/models/Goals.py
@@ -0,0 +1,31 @@
+import pygame
+
+
+class Goal:
+    def __init__(self, x1, y1, x2, y2):
+        self.x1 = x1
+        self.y1 = y1
+
+        self.x2 = x2
+        self.y2 = y2
+
+        self.active = True
+
+    def draw(self, win):
+        pygame.draw.rect(win, (0, 255, 0), (self.x1, self.y1,
+                         self.x2 - self.x1, self.y2 - self.y1), 2)
+        if self.active:
+            pygame.draw.rect(win, (0, 255, 0), (self.x1, self.y1,
+                             self.x2 - self.x1, self.y2 - self.y1), 2)
+
+
+def getGoals():
+    goals = []
+
+    for i in range(200, 650, 50):
+        goals.append(Goal(i + 20, 240, i + 30, 260))
+
+        # Bottom Side
+        # goals.append(Wall(i, 350, i, 400))
+
+    return goals
